d185bb868890eb7e6e5bc7479cd7c20a6fc174db
ashleylovette <ashleylovette15@gmail.com>
2022-03-15 15:30:31 -0500

    added form

--- app/controllers/vegetables_controller.rb
+++ app/controllers/vegetables_controller.rb
@@ -2,4 +2,16 @@ class VegetablesController < ApplicationController
     def show
         @veg = Vegetable.find(params[:id])
     end
+
+    def index
+        @veggies = Vegetable.all
+    end
+
+    def new
+
+    end
+
+    def create
+        
+    end
 end
