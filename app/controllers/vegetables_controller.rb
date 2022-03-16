f802d2d777dea155bb9cfa150ff38bad9e465bad
ashleylovette <ashleylovette15@gmail.com>
2022-03-15 19:41:53 -0500

    CRUD operations

--- app/controllers/vegetables_controller.rb
+++ app/controllers/vegetables_controller.rb
@@ -15,10 +15,31 @@ class VegetablesController < ApplicationController
     @veg = Vegetable.new(params.require(:vegetable).permit(:name, :season))
 
     if @veg.save
+      flash[:notice] = "Vegetable added!"
       redirect_to @veg
     else
       render 'new'
     end
-    
+  end
+
+  def edit
+    @veg = Vegetable.find(params[:id])
+  end
+
+  def update
+    @veg = Vegetable.find(params[:id])
+    if @veg.update(params.require(:vegetable).permit(:name, :season))
+      flash[:message] = "Vegetable details updated!"
+      redirect_to @veg
+    else 
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @veg = Vegetable.find(params[:id])
+    flash[:message] = "#{@veg.name} was successfully deleted!"
+    @veg.destroy
+    redirect_to vegetables_path
   end
 end
