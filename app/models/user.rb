4081e2b66ffd027c9bc4f197af7624e9c663ad1c
ashleylovette <ashleylovette15@gmail.com>
2022-03-31 16:36:46 -0500

    styling-add-password-digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :vegetables
   validates :username, uniqueness: true, presence: true, length: {minimum: 3, maximum: 25}
   validates :email, uniqueness: true, presence: true, format: {with: URI::MailTo::EMAIL_REGEXP}
