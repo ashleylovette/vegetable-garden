696b45adf912dcbbbbc71a71e5b82f9cc595ebc8
ashleylovette <ashleylovette15@gmail.com>
2022-04-01 11:18:42 -0500

    add users-backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,18 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to Vegetable Garden #{@user.username}! You have successfully signed up"
+      redirect_to vegetables_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
