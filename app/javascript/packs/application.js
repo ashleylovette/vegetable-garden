f821f8597091c33bf42be3d81fcd6984128f8e31
ashleylovette <ashleylovette15@gmail.com>
2022-04-12 22:15:21 -0500

    navbar-styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,8 @@ require("channels")
 // require("bootstrap")
 import "bootstrap"
 
+FontAwesome.config.mutateApproach = 'sync'
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
